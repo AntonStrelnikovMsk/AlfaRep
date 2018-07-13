7136cc3f9116a533f46c5a3b78871a3cc371017a
alexandr.voronin <alexandr.voronin>
2018-07-13 19:07:57 +0300

    'test'

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-	bot.send_message(message.chat.id, 'Вау правда?')
+	bot.send_message(message.chat.id, 'Время пришло?')
 
 if __name__ == '__main__':
 	bot.polling(none_stop=True)
